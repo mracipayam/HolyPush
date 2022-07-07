c6d7b9fffd92779fbb77fdf6f235fb6e4aca6f36
Murat Han Acıpayam <mracipayam@gmail.com>
2022-07-07 11:41:56 +0300

    Character Added
    Character Added and animated.

---
+++ Assets/_GameData/Scripts/CharacterMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody        _rigidbody;
+    [SerializeField] private FloatingJoystick joystick;
+    [SerializeField] private Animator         _animator;
+    [SerializeField] private float            _moveSpeed;
+
+    private void FixedUpdate()
+    {
+
+        _rigidbody.velocity = new Vector3(joystick.Horizontal, 0, joystick.Vertical) * _moveSpeed;
+
+        if (joystick.Horizontal != 0 || joystick.Vertical != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(_rigidbody.velocity);
+            _animator.SetBool("isRunning",true);
+        }
+        else
+        {
+            _animator.SetBool("isRunning",false);
+        }
+
+        
+    }
+}
