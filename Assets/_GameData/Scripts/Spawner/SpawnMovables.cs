44706174e5f33ed51ccaeabd7a0d1f93b26b86cf
mracipayam <mracipayam@gmail.com>
2022-07-16 21:26:57 +0300

    update spawn movables

--- Assets/_GameData/Scripts/Spawner/SpawnMovables.cs
+++ Assets/_GameData/Scripts/Spawner/SpawnMovables.cs
@@ -17,15 +17,8 @@ public class SpawnMovables : MonoBehaviour
         timerValue -= Time.deltaTime;
         if (timerValue <= 0)
         {
-            var movableItem = Instantiate(MovableItems[Random.Range(0,MovableItems.Count)], new Vector3(Random.Range(-7f,7f), 3, Random.Range(-20f,0f)), Quaternion.identity);
-            if(movableItem.gameObject == MovableItems[3].gameObject)
-            {
-                movableItem.transform.rotation = Quaternion.Euler(-90, 0, 0);
-            }
-            else
-            {
-                movableItem.transform.rotation = Quaternion.Euler(0, 0, 0);
-            }
+            Instantiate(MovableItems[Random.Range(0,MovableItems.Count)], new Vector3(Random.Range(-7f,7f), 3, Random.Range(-20f,0f)), Quaternion.identity);
+            
             timerValue = Timer;
         }
     }
